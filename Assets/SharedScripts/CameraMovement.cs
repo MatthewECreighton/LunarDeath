9201fc584bddb2bcaffadca5022d4d2be0070824
MatthewECreighton <matthew.e.creighton@gmail.com>
2020-11-13 11:42:24 -0700

    Fix an issue with camera rotation;

---
+++ Assets/SharedScripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform ObjectToFollow;
+    public float HeightOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // camera -6m behind, 1m above
+        transform.position = new Vector3(ObjectToFollow.position.x, ObjectToFollow.position.y + HeightOffset, transform.position.z);
+    }
+}
