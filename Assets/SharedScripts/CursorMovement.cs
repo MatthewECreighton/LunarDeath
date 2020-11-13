76e564bde3dd84dc9edcea0212582e98eab6eb64
MatthewECreighton <matthew.e.creighton@gmail.com>
2020-11-13 10:40:59 -0700

    Get basic mouse object following working;

---
+++ Assets/SharedScripts/CursorMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var hit = new RaycastHit();
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hits = Physics.RaycastAll(ray, 1000f, 1);
+
+        if (hits.Length == 1 && hits[0].transform.tag == "Space") 
+        {
+            var hitPosition = hits[0].point;
+            transform.position = hitPosition;
+        }
+    }
+}
