3ece7ab5dfc2286d6295a3adfcd0eaa45e9dcf81
MatthewECreighton <matthew.e.creighton@gmail.com>
2020-11-13 11:32:49 -0700

    Apply force to rocket on click;

---
+++ Assets/SharedScripts/RocketMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    public float forceMultiplier = 2;
+
+    private Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left mouse click
+        if (Input.GetMouseButton(0))
+        {
+            myRigidbody.AddForce(new Vector3(0, 10, 0) * forceMultiplier, ForceMode.Acceleration);
+        }
+    }
+}
