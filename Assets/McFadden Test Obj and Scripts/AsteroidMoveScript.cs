26dd7cac802532d8c66a6e404e37b41899e26f0b
Mike <mcfadden4949@gmail.com>
2020-11-27 15:36:43 -0500

    Asteroids
    Added 4 versions of basic asteroids

--- Assets/McFadden Test Obj and Scripts/AsteroidMoveScript.cs
+++ Assets/McFadden Test Obj and Scripts/AsteroidMoveScript.cs
@@ -10,11 +10,19 @@ public class AsteroidMoveScript : MonoBehaviour
     public float scale = 1.0f;
     private float randSp;
     
+    private Vector3 RandomVector(float min, float max) 
+        {
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        var z = Random.Range(min, max);
+        return new Vector3(x, y, z);
+        }
+    
     // Start is called before the first frame update
     void Start()
     {
         scale = Random.Range(.25f,3.0f);
-        randSp = Random.Range(.1F, 2.0F);
+        //randSp = Random.Range(.1F, 2.0F);
         
         //get random size (need to be Vector3 not Vector2) if you want to just change x scale 
         Vector3 randomSize = new Vector3 (scale,scale,scale);
@@ -22,8 +30,11 @@ public class AsteroidMoveScript : MonoBehaviour
         //set it to the scale of previously instantiated platform 
         transform.localScale = randomSize;
 
-        //set random speed 
-        stepSpeed *= Random.Range(1,3);
+
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = RandomVector(0f, 5f);
+        /*//set random speed 
+        stepSpeed *= Random.Range(1,3);*/
 
         StartCoroutine(moving());
     }
@@ -31,9 +42,9 @@ public class AsteroidMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
-        var pos = transform.position;
+        /*var pos = transform.position;
         pos.x += speed;
-        transform.position = pos;
+        transform.position = pos;*/
 
         //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         //transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, rotationSpeed * Time.deltaTime);   
@@ -41,7 +52,7 @@ public class AsteroidMoveScript : MonoBehaviour
 
     IEnumerator moving()
     {        
-        var pos = transform.position;
+        /*var pos = transform.position;
         pos.x += speed;
         transform.position = pos;
 
@@ -49,7 +60,7 @@ public class AsteroidMoveScript : MonoBehaviour
         if(pos.x < -5)
         {
             speed =0;
-        }
+        }*/
 
         yield return new WaitForSeconds(stepSpeed);   
 
---
+++ Assets/McFadden Test Obj and Scripts/Asteroids/AsteroidControllerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidControllerScript : MonoBehaviour
+{
+    private float max = 40;
+    private int speed = 1;
+    public float stepSpeed = 1.0F;
+    public float scale = 1.0f;
+    private float randSp;
+    
+    private Vector3 RandomVector(float min, float max) 
+    {
+    var x = Random.Range(min, max);
+    var y = Random.Range(min, max);
+    var z = Random.Range(0f,0f);
+    return new Vector3(x, y, z);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = Random.Range(.25f,3.0f);
+        randSp = Random.Range(.1F, .5F);
+        
+        //get random size (need to be Vector3 not Vector2) if you want to just change x scale 
+        Vector3 randomSize = new Vector3 (scale,scale,scale);
+        
+        //set it to the scale of previously instantiated platform 
+        transform.localScale = randomSize;
+        transform.GetComponent<Rigidbody>().mass = scale;
+        transform.rotation = Random.rotation;
+
+
+        var rb = GetComponent<Rigidbody>();
+        rb.velocity = RandomVector(-5f, 5f)*randSp;
+        /*//set random speed 
+        stepSpeed *= Random.Range(1,3);*/
+
+        //StartCoroutine(moving());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*IEnumerator moving()
+    {      
+        yield return new WaitForSeconds(stepSpeed);   
+
+        StartCoroutine(moving());
+    }*/
+}
